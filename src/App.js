20b46d5f49e8439559aa3f444211b012f7fb7a96
Molly Huang <molly.h@gmail.com>
2021-02-26 21:07:19 +0800

    #8-4. Intro To JSX & Fragments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { render } from "@testing-library/react";
-import React, { Fragment, Component } from "react";
+import React, { Component } from "react";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <>
+      <div className="app">
         <h1>Hello from React</h1>
         <h2>Goodbye</h2>
-      </>
+      </div>
     );
   }
 }
