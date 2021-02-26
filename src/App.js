dc9cefded58ac2744fc5cf398ba8434e34007765
Molly Huang <molly.h@gmail.com>
2021-02-26 21:05:14 +0800

    #8-3. Fragment 第3種寫法-但不建議

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <>
         <h1>Hello from React</h1>
         <h2>Goodbye</h2>
-      </Fragment>
+      </>
     );
   }
 }
