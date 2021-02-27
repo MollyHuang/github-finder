0e5ad6cbcbecc65ade24f7ea796c3c12a8255654
Molly Huang <molly.h@gmail.com>
2021-02-27 12:05:39 +0800

    #11-2. constructor(){...}  可縮寫成 state = {...}

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,14 +2,11 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: '1',
-      login: "mojombo",
-      avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-      html_url: "https://github.com/mojombo"
-    }
+  state = {
+    id: '1',
+    login: "mojombo",
+    avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+    html_url: "https://github.com/mojombo"
   }
 
   render() {
