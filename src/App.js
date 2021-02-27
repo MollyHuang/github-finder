fc3ff80c3ce714e53dd5169f6e3a31ea4f00df90
Molly Huang <molly.h@gmail.com>
2021-02-27 11:10:51 +0800

    #9. Expressions & Conditionals In JSX

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const name = "John Doe";
+    const loading = false;
+    const showName = true;
+
     return (
       <div className="app">
-        <h1>Hello from React</h1>
-        <h2>Goodbye</h2>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
       </div>
     );
   }
