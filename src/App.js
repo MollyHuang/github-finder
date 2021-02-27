4f336ee4aa76c40e7139347ec1a8d26439fd194b
Molly Huang <molly.h@gmail.com>
2021-02-27 16:55:24 +0800

    #19. Clear Users From State

--- src/App.js
+++ src/App.js
@@ -14,30 +14,33 @@ class App extends Component {
 
   // async componentDidMount() {
   //   this.setState({ loading: true });
-
   //   // clg + tab
   //   // console.log(process.env);
-
   //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECTET}`);
-
   //   this.setState({ users: res.data, loading: false });
   // }
 
   searchUsers = async text => {
     this.setState({ loading: true });
-
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECTET}`);
-
     this.setState({ users: res.data.items, loading: false });
   }
 
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false })
+
   render() {
+    const { users, loading } = this.state;
+
     return (
       <div className="app">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     )
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -12,15 +12,25 @@ export class Search extends Component {
     this.props.searchUsers(this.state.text);
     this.setState({ text: '' });
   }
+
   onChange = e => this.setState({ [e.target.name]: e.target.value })
 
   render() {
+    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
-        <form className="form" onSubmit={this.onSubmit}>
-          <input name="text" type="text" value={this.state.text} onChange={this.onChange} placeholder="Search Users..." />
+        <form onSubmit={this.onSubmit} className="form">
+          <input name="text" type="text" placeholder="Search Users..."
+            value={this.state.text}
+            onChange={this.onChange}
+          />
           <input type="submit" value="Search" className='btn btn-dark btn-block' />
         </form>
+        {showClear && (
+          <button className='btn btn-light btn-block'
+            onClick={clearUsers}>Clear</button>
+        )}
       </div>
     )
   }
