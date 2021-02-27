3176ff0ecdf85384be036a2412191f3a52fe55c1
Molly Huang <molly.h@gmail.com>
2021-02-27 14:46:06 +0800

    #14. HTTP Requests & Updating State - Axios - fetch users

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import { render } from "@testing-library/react";
-import React from "react";
-import './App.css';
+import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import './App.css';
+import axios from "axios";
 
-const App = () => {
-  return (
-    <div className="app">
-      <Navbar />
-      <div className="container">
-        <Users />
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Navbar />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
-    </div>
-  );
-};
+    )
+  }
+}
 
 export default App;
