47015b1ad482583f5365b95758311644397d0e13
Molly Huang <molly.h@gmail.com>
2021-02-28 00:20:53 +0800

    #34-2. Home & NotFound Components - Add NotFound page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import About from "./components/pages/About";
 import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import User from "./components/users/User";
 import Alert from "./components/layout/Alert";
 
@@ -25,6 +26,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
